518e0bd7aeea1d07d3d8f35e20e09ae0cabe561d
Mathoo-code <mathewrolf16@gmail.com>
2025-05-01 22:52:28 -0700

    44:08 in React JS Full Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+
+const Card = ({ title, rating }) => {
+
+
+  const[count,setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  //[variableName, setvariableName]
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked]);
+
+
+  return(
+    <div className='card' onClick={() => setCount((prevState) => prevState + 1)}>
+      <h2>{title} - {count || null}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '♡'}
+      </button>
+    </div>
+  )
+}
+// Reuse Card Component in App
+
+
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='card-container'>
+      <h2>Movie Ratings</h2>
+     
+      <Card title="Star Wars" rating ={5}/>
+      <Card title="Avatar" rating ={4}/>
+      <Card title="Lion King" rating ={5}/>
+      
+    </div>
+    
   )
 }
 
 export default App
+
+
